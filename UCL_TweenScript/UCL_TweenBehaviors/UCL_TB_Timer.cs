1db53f7e53727db5197080803104610dacdb10da
tim099 <tim11251994@gmail.com>
2020-09-26 17:38:54 +0800

    Update UCL_TB_Timer.cs

--- UCL_TweenScript/UCL_TweenBehaviors/UCL_TB_Timer.cs
+++ UCL_TweenScript/UCL_TweenBehaviors/UCL_TB_Timer.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 namespace UCL.TweenLib {
     public class UCL_TB_Timer : UCL_TB_Tweener {
-        public Text m_TimerText;
+        public Text m_TimerText = null;
 
         /// <summary>
         /// Call once on time update
         /// </summary>
-        public UnityEngine.Events.UnityEvent m_UpdateEvent;
+        public UnityEngine.Events.UnityEvent m_UpdateEvent = null;
 
         /// <summary>
         /// Call once on time update if Time <= m_Threshold
         /// </summary>
-        public UnityEngine.Events.UnityEvent m_UpdateUnderThresholdEvent;
+        public UnityEngine.Events.UnityEvent m_UpdateUnderThresholdEvent = null;
+        public UCL.Core.UCL_FloatEvent m_TimeUpdateEvent = null;
         public int m_Threshold = 0;
 
-        protected int m_Time;
+        protected int m_Time = 0;
         public override void StartTweener() {
             TimerUpdate(Mathf.CeilToInt(m_Duration));
             CreateTweener().OnUpdate((y)=> {
                 int time = Mathf.CeilToInt(m_Duration - m_Tweener.Timer);
+                if(m_TimeUpdateEvent != null) m_TimeUpdateEvent.Invoke(1f-y);
                 while(m_Time > time) {
                     TimerUpdate(m_Time - 1);
                 }
@@ -36,7 +38,6 @@ namespace UCL.TweenLib {
             }
             if(m_UpdateEvent != null) m_UpdateEvent.Invoke();
             if(m_Time <= m_Threshold) {
-                //Debug.LogWarning("m_Time <= m_Threshold m_Time:" + m_Time+ ",m_Threshold:"+ m_Threshold);
                 if(m_UpdateUnderThresholdEvent != null) m_UpdateUnderThresholdEvent.Invoke();
             }
         }
